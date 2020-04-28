9a998f00c25a7bef43d011ae6f30e2835d44d4fb
Sauravpersonal08 <saurav.official08@gmail.com>
2020-04-28 19:42:12 +0530

    First commited code

---
+++ src/test/java/testAutomation/OpenBrowserGoogle.java
@@ -0,0 +1,19 @@
+package testAutomation;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OpenBrowserGoogle {
+	
+	@Test
+	public void OpenBrowser() throws Exception{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ssp34\\Desktop\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.navigate().to("http://www.google.com");
+	}
+
+}
